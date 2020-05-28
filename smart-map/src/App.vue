79c37eee64e1821aaa90e55750e1bb40d4ce8222
Alexei Volynshchikov <alex.volynshchikov@techusage.com>
2020-05-28 16:43:51 +0700

    add smartmap

---
+++ smart-map/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MainMap msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MainMap from './components/MainMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainMap
+  }
+}
+</script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
---
+++ smart-map/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import "leaflet/dist/leaflet.css"
+import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
+import 'leaflet-fullscreen/dist/Leaflet.fullscreen';
+
+import L from 'leaflet';
+
+// delete L.Icon.Default.prototype._getIconUrl;
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+// });
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
