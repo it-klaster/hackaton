aa3e166f2e2347066ab3c888e767ea522742ce0d
Nik Volkov <nick.volkov@techusage.com>
2020-05-30 14:30:26 +0700

    update models to dump

--- python_apps/bot/models.py
+++ python_apps/bot/models.py
@@ -1,5 +1,6 @@
 import textwrap
 
+from sqlalchemy.dialects.mysql import TINYINT
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, UniqueConstraint, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -16,52 +17,115 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'bot_users'
 
     id = Column(Integer, primary_key=True)
+    name = Column(String)
     telegramm_id = Column(String, nullable=False, unique=True)
-    address_id = Column(Integer, ForeignKey('addresses.id'))
-    entrance = Column(Integer)
+    address_id = Column(Integer, ForeignKey('bot_addreses.id'))
+    entrance = Column(Integer)  # Not exist in db
+    subscribe = Column(TINYINT(1))
+    phone = Column(String)
 
     def __repr__(self):
         return f'<User {self.telegramm_id}>'
 
 
 class Address(Base):
-    __tablename__ = 'addresses'
-    __table_args__ = (
-        UniqueConstraint('street', 'house'),
-    )
+    __tablename__ = 'bot_addreses'
 
     id = Column(Integer, primary_key=True)
-    street = Column(String(256), nullable=False)
-    house = Column(String(50), nullable=False)
+    name = Column(String(256), nullable=False)
 
     def __repr__(self):
-        return f'<Address {self.street}, {self.house}>'
+        return f'<Address {self.name}>'
 
 
 class Event(Base):
-    __tablename__ = 'events'
+    __tablename__ = 'bot_events'
 
     id = Column(Integer, primary_key=True)
-    msg = Column(String, nullable=False)
-    importance = Column(Integer)
-    start_date = Column(DateTime, nullable=False)
-    finish_date = Column(DateTime)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('bot_users.id'))
+    event_id = Column(Integer, ForeignKey('bot_idevents.id'))
+    timer_id = Column(Integer, ForeignKey('bot_etimer.id'))
 
     def __repr__(self):
-        short_msg = textwrap.shorten(self.msg, width=50, placeholder="...")
-        return f'<Event {self.start_date} {short_msg}>'
+        short_msg = textwrap.shorten(self.name, width=50, placeholder="...")
+        return f'<Event {short_msg}>'
 
 
-class SentEvent(Base):
-    __tablename__ = 'sent_events_log'
+class EventType(Base):
+    __tablename__ = 'bot_idevents'
 
     id = Column(Integer, primary_key=True)
-    sent_at = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    event_id = Column(Integer, ForeignKey('events.id'))
+    name = Column(String, nullable=False)
+    # alert_id = Column(Integer, ForeignKey())
+    status_id = Column(Integer, ForeignKey('bot_estatus.id'))
+    address_id = Column(Integer, ForeignKey('bot_addreses.id'))
 
-    def __repr__(self):
-        return f'<SentEvent {self.sent_at} {self.user}>'
+
+class MsgLog(Base):
+    __tablename__ = 'bot_msglog'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('bot_users.id'))
+    mag_text = Column(String)
+
+
+class AdminUser(Base):
+    __tablename__ = 'bot_admins'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    telegramm_id = Column(Integer, nullable=False, unique=True)
+    org_id = Column(Integer, ForeignKey('bot_org.id'))
+    rules_id = Column(Integer, ForeignKey('bot_rules.id'))
+
+
+class Organization(Base):
+    __tablename__ = 'bot_org'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Rules(Base):
+    __tablename__ = 'bot_rules'
+
+    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, ForeignKey('bot_idevents.id'))
+    organization_id = Column(Integer, ForeignKey('bot_org.id'))
+
+
+class EventLog(Base):
+    __tablename__ = 'bot_elog'
+
+    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, ForeignKey('bot_idevents.id'))
+    operator_id = Column(Integer)   # TODO: Foreignkey
+
+
+class EventStatus(Base):
+    __tablename__ = 'bot_estatus'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class EventTimer(Base):
+    __tablename__ = 'bot_etimer'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    start_time = Column(DateTime)
+    stop_time = Column(DateTime)
+    send_time = Column(DateTime)
+    repeat = Column(TINYINT(1))
+
+
+class EventWeight(Base):
+    __tablename__ = 'bot_eweight'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
